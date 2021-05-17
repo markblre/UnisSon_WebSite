fc7ffd49271798c8dd2b984952ae5fbd573a13a1
Mark Ballereau <mark.b@live.fr>
2021-05-17 13:00:32 +0200

    Deletion of files that are not needed for the site to function.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-
-gulp.task('sass', function () {
-  return gulp.src("./src/app.scss")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
